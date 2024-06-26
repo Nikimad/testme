2832d384f179ff5385da5fb4f9601d915a8e16bf
Nikimad <PybePoiD@gmail.com>
2024-06-26 15:34:14 +0300

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
 const defineNextConfig = (config) => config;
 
-export default defineNextConfig({});
+export default defineNextConfig({
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://interns-test-fe.snp.agency/api/v1/:path*",
+      },
+    ];
+  },
+  reactStrictMode: false
+});
