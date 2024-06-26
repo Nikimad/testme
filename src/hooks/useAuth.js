cb1135043c4c43975e24eb842e5e5b742d8a6e5b
Nikimad <PybePoiD@gmail.com>
2024-06-26 15:42:41 +0300

    Add src/hooks/useAuth.js

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import useAction from "./useAction";
+import { authActions } from "@/models/auth";
+import { authSelectors } from "@/models/auth/selectors";
+
+const useAuth = () => {
+    const isUserAuthorized = useSelector(authSelectors.selectIsUserAuthorized);
+    const getUser = useAction(authActions.getUser);
+
+    useEffect(() => {
+        if (!isUserAuthorized) getUser();
+    }, []);
+};
+
+export default useAuth;
