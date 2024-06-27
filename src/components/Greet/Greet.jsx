a775c39274fce96d821318417a87ea7a35601075
Nikimad <PybePoiD@gmail.com>
2024-06-27 13:06:49 +0300

    Add src/components/Greet

---
+++ src/components/Greet/Greet.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import LayoutContainer from "../LayoutContainer";
+import Button from "../Button";
+import s from "./Greet.module.scss";
+
+const Greet = () => (
+  <LayoutContainer element="main" className={s.container}>
+    <div className={s.container__content}>
+      <h2 className={s.container__title}>Discover the Joy of Learning</h2>
+      <p className={s.container__text}>
+      Education is the key to unlocking a world of opportunities. It is a journey that empowers individuals to reach their full potential and make a positive impact on society. In today's fast-paced and ever-changing world, education plays a crucial role in shaping the future.
+      </p>
+      <Button className={s.container__button}>Geting start</Button>
+    </div>
+    <Image className={s.container__img} width={500} height={500} src="/main.svg" alt="Charts" aria-hidden={true} />
+  </LayoutContainer>
+);
+
+export default Greet;
