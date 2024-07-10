fd2671537532091363b2044e51718f57600adc45
Nikimad <PybePoiD@gmail.com>
2024-07-10 16:56:37 +0300

    Change src/components/TestsList

--- src/components/TestsList/TestsList.jsx
+++ src/components/TestsList/TestsList.jsx
@@ -1,4 +1,4 @@
-import Link from "../Link";
+import TestLink from "../TestLink";
 import s from "./TestsList.module.scss";
 
 const Tests = ({ tests }) => (
@@ -9,7 +9,7 @@ const Tests = ({ tests }) => (
       <ul className={s.tests__list}>
         {tests.map((test) => (
           <li key={test.id} className={s.tests__list__item}>
-            <Link href={test.title} className={s.tests__list__link}>{test.title}</Link>
+            <TestLink href={String(test.id)} className={s.tests__list__link}>{test.title}</TestLink>
             <span>{new Date(test.created_at).toDateString()}</span>
           </li>
         ))}
