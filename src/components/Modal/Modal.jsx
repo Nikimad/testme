f836ed951a1948f755aca503b8d03ab17890d8e2
Nikimad <PybePoiD@gmail.com>
2024-07-20 16:49:04 +0300

    Change src/components/Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,25 @@
+import LayoutContainer from "../LayoutContainer";
 import s from "./Modal.module.scss";
 
 const Modal = ({ overlay, modal, onClose, onDismiss, title, children }) => (
   <div className={s.modal__overlay} ref={overlay} onClick={onDismiss}>
-    <div
+    <LayoutContainer
+      element="div"
       aria-modal="true"
       role="dialog"
       tabIndex="-1"
-      ref={modal}
+      innerRef={modal}
       className={s.modal__content}
     >
       <header className={s.modal__header}>
         <button className={s.modal__button} onClick={onClose}>
           <span className="visually-hidden">Close modal</span>
-          <span className={s.modal__button__mark} aria-hidden={true}>+</span>
+          <span className={s.modal__button__mark} aria-hidden={true}>&#x2715;</span>
         </button>
-        <h2>{title}</h2>
+        <h2 className={s.modal__title}>{title}</h2>
       </header>
       <main>{children}</main>
-    </div>
+    </LayoutContainer>
   </div>
 );
 
