968eb66129938f3e56f71e225c482d0db6c95c44
Nikimad <PybePoiD@gmail.com>
2024-07-28 16:09:27 +0300

    Change src/components/Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,31 +5,37 @@ import Button from "../Button";
 import s from "./Dashboard.module.scss";
 
 const Dashboard = ({ onStart, isLoading }) => (
-  <Placeholder isLoading={isLoading}>
-    <LayoutContainer element="section" className={s.container}>
-      <div className={s.container__content}>
-        <h2 className={s.container__title}>Discover the Joy of Learning</h2>
-        <p>
-          Education is the key to unlocking a world of opportunities. It is a
-          journey that empowers individuals to reach their full potential and
-          make a positive impact on society. In today's fast-paced and
-          ever-changing world, education plays a crucial role in shaping the
-          future.
-        </p>
-        <Button styleType="pill" className={s.container__button} onClick={onStart}>
-          Getting start
-        </Button>
+  <LayoutContainer element="section">
+    <Placeholder isLoading={isLoading} isFullHeight={true}>
+      <div className={s.container}>
+        <div className={s.container__content}>
+          <h2 className={s.container__title}>Discover the Joy of Learning</h2>
+          <p>
+            Education is the key to unlocking a world of opportunities. It is a
+            journey that empowers individuals to reach their full potential and
+            make a positive impact on society. In today's fast-paced and
+            ever-changing world, education plays a crucial role in shaping the
+            future.
+          </p>
+          <Button
+            styleType="pill"
+            className={s.container__button}
+            onClick={onStart}
+          >
+            Getting start
+          </Button>
+        </div>
+        <Image
+          className={s.container__img}
+          width={500}
+          height={500}
+          src="/main.svg"
+          alt="Charts"
+          aria-hidden={true}
+        />
       </div>
-      <Image
-        className={s.container__img}
-        width={500}
-        height={500}
-        src="/main.svg"
-        alt="Charts"
-        aria-hidden={true}
-      />
-    </LayoutContainer>
-  </Placeholder>
+    </Placeholder>
+  </LayoutContainer>
 );
 
 export default Dashboard;
