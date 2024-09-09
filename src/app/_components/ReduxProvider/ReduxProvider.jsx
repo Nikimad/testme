3152c8dc2d02fbf2f8c73aa3f776954e16278703
Nikimad <PybePoiD@gmail.com>
2024-09-09 12:57:25 +0300

    Change src/app/_components/ReduxProvider

--- src/app/_components/ReduxProvider/ReduxProvider.jsx
+++ src/app/_components/ReduxProvider/ReduxProvider.jsx
@@ -1,15 +1,11 @@
-'use client';
-
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore } from '@/models';
-import { authActions } from '@/models/auth';
 
 const ReduxProvider = ({ children }) => {
   const storeRef = useRef();
   if (!storeRef.current) {
     storeRef.current = makeStore();
-    storeRef.current.dispatch(authActions.getUser());
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>
