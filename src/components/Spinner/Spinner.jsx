64cdd80c2be9247756517ab75bac0276721475c1
Nikimad <PybePoiD@gmail.com>
2024-09-10 13:29:17 +0300

    Change src/components/Spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,13 +1,10 @@
 import cn from "classnames";
 import s from "./Spinner.module.scss";
 
-const Spinner = ({ action, className }) => (
-  <span
-    className={cn(s.spinner, className ?? "")}
-    aria-hidden="true"
-  >
+const Spinner = ({ className, children }) => (
+  <span className={cn(s.spinner, className ?? "")} aria-hidden="true">
     <span className="visually-hidden" aria-live="polite">
-      {action} is loading
+      {children} is loading
     </span>
   </span>
 );
