83c28d3cb357adfee395c5316351d3416d1ac4aa
Nikimad <PybePoiD@gmail.com>
2024-10-18 14:41:04 +0300

    Change src/components/Field

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,26 +1,24 @@
 import cn from "classnames";
-import { Field as FormikField, ErrorMessage } from "formik";
+import Input from "../Input";
+import Error from "../Error";
 import s from "./Field.module.scss";
 
-const Field = ({ isInvalid, label, name, ...props }) => (
+const Field = ({ isLabelHidden, label, name, ...props }) => (
   <div className={s.field}>
-    <label htmlFor={`${name}_field`} className={s.field__label}>
+    <label
+      htmlFor={`${name}_field`}
+      className={cn(s.field__label, { ["visually-hidden"]: isLabelHidden })}
+    >
       {label}
     </label>
-    <FormikField
+    <Input
+      key={name}
       id={`${name}_field`}
       name={name}
       aria-describedby={`${name}_field-error`}
       {...props}
-      className={cn(s.field__input, { [s.field__input_invalid]: isInvalid })}
     />
-    <span
-      id={`${name}_field-error`}
-      aria-live="polite"
-      className={s.field__error}
-    >
-      <ErrorMessage name={name} />
-    </span>
+    <Error name={name} id={`${name}_field-error`} />
   </div>
 );
 
