5c349ba33bab86a89e41b7b3e281dd95759bae59
Nikimad <PybePoiD@gmail.com>
2024-10-18 14:38:49 +0300

    Change src/hooks/useModal.js

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,10 +1,34 @@
-import { useContext } from "react";
-import { ModalContext } from "@/components/ModalProvider/ModalProvider";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import InertContext from "@/context/InertContext";
 
 const useModal = (openerRef) => {;
-  const modalProps = useContext(ModalContext);
+  const { setInert, removeInert } = useContext(InertContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isModalWasClosed = useRef(false);
 
-  return modalProps;
+  const openModal = useCallback(() => {
+    setIsModalOpen(true);
+    setInert();
+    isModalWasClosed.current = false;
+  }, [setInert, isModalWasClosed]);
+
+  const closeModal = useCallback(() => {
+    setIsModalOpen(false);
+    removeInert();
+    isModalWasClosed.current = true;
+  }, [removeInert, isModalWasClosed]);
+
+  useEffect(() => {
+    isModalWasClosed.current && openerRef.current?.focus();
+  }, [isModalWasClosed.current, openerRef])
+
+  useEffect(() => {
+    return () => {
+      isModalOpen && removeInert();
+    }
+  }, [isModalOpen, removeInert])
+
+  return { isModalOpen, openModal, closeModal };
 };
 
 export default useModal;
