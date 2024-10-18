b2164818ae82997b3cdc027517cb1e9312d08532
Nikimad <PybePoiD@gmail.com>
2024-10-18 14:50:00 +0300

    Change src/components: clear

--- src/components/Link/Link.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import cn from "classnames";
-import NextLink from "next/link";
-import s from "./Link.module.scss";
-
-const Link = ({ className, linkref, ...props }) => <NextLink ref={linkref} className={cn(s.link, className ?? "")}  {...props} />;
-
-export default Link;
