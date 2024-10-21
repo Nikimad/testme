e42670bbdfa01ec7b181b8e736a4f2c25b2592ee
Nikimad <PybePoiD@gmail.com>
2024-10-21 10:28:09 +0300

    Change src/app/(home)/@user/_components/Tests

--- src/app/(home)/@user/_components/Tests/Tests.jsx
+++ src/app/(home)/@user/_components/Tests/Tests.jsx
@@ -1,11 +1,24 @@
 "use client";
 
-import { useAppSelector } from "@/models/hooks";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { useAction, useAppSelector } from "@/models/hooks";
 import { testsSelectors } from "@/models/tests/selectors";
+import { testsActions } from "@/models/tests";
 import Test from "../Test";
 
 const Tests = () => {
   const tests = useAppSelector(testsSelectors.selectTests);
+  const lastQuery = useAppSelector(testsSelectors.selectQuery);
+  const currentQuery = useSearchParams().toString();
+
+  const getTests = useAction(testsActions.getTests);
+
+  useEffect(() => {
+    if (lastQuery === null) {
+      getTests(currentQuery);
+    }
+  }, [lastQuery, currentQuery, getTests]);
 
   return tests.map((test) => <Test key={test.id} test={test} />);
 };
