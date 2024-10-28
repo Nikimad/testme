87d6af3ebbf0aba2d9f6c5139d082fa7416ada89
Nikimad <PybePoiD@gmail.com>
2024-10-28 11:39:49 +0300

    Change src/lib/_fetch.js

--- src/lib/_fetch.js
+++ src/lib/_fetch.js
@@ -42,7 +42,42 @@ const _fetch = async (method, url, headers = {}, options = {}) => {
   if (!response.ok) throw data;
 };
 
+const _experemental_fetch = async (
+  method,
+  url,
+  headers = {},
+  options = {}
+) => {
+  console.log(`${method}: ${url}`);
+  const response = await fetch(`${apiURL}/${url}`, {
+    method,
+    credentials: "include",
+    ...handleOptions(options),
+    headers: {
+      "Content-Type": "application/json",
+      "Scope-Key": "8TSKR76EQVOX45",
+      ...headers,
+    },
+  });
+
+  let error = null;
+  let data = null; 
+
+  try {
+    data = await response.json();
+  } catch {
+    error = { error: "Something went wrong" };
+  }
+
+  return [error, data];
+};
+
 export const _get = (...args) => _fetch(methods.get, ...args);
 export const _post = (...args) => _fetch(methods.post, ...args);
 export const _patch = (...args) => _fetch(methods.patch, ...args);
 export const _delete = (...args) => _fetch(methods.delete, ...args);
+
+export const _experemental_get = (...args) => _experemental_fetch(methods.get, ...args);
+export const _experemental_post = (...args) => _experemental_fetch(methods.post, ...args);
+export const _experemental_patch = (...args) => _experemental_fetch(methods.patch, ...args);
+export const _experemental_delete = (...args) => _experemental_fetch(methods.delete, ...args);
