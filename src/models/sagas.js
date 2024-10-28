f7dfc94643256494fac6dd5c52e3c8bd97e59976
Nikimad <PybePoiD@gmail.com>
2024-10-28 14:21:56 +0300

    Change src/models/sagas.js

--- src/models/sagas.js
+++ src/models/sagas.js
@@ -1,9 +1,10 @@
 import { all } from "redux-saga/effects";
+import constructorSaga from "./constructor/sagas";
 import authhorizationSaga from "./authorization/sagas";
 import testsSaga from "./tests/sagas";
 
 function* rootSaga() {
-  yield all([authhorizationSaga(), testsSaga()]);
+  yield all([constructorSaga(), authhorizationSaga(), testsSaga()]);
 }
 
 export default rootSaga;
