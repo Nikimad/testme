f35174f73e2e7611398ed89581f5cb74aa95ffe9
Nikimad <PybePoiD@gmail.com>
2024-10-28 14:18:16 +0300

    Change src/app/(home)/@user/test/(constructor)/[testId]/constructor/page

--- src/app/(home)/@user/test/(constructor)/[testId]/constructor/page.jsx
+++ src/app/(home)/@user/test/(constructor)/[testId]/constructor/page.jsx
@@ -1,1 +1,19 @@
-export default () => null;
+import QuestionEditor from "./_components/QuestionEditor";
+import QuestionsList from "./_components/QuestionsList";
+
+const ConstructorPage = () => (
+  <>
+  <QuestionEditor />
+  <QuestionsList />
+  <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <button type="submit" className="pill">
+      Save
+    </button>
+    <button type="reset" className="pill">
+      Delete
+    </button>
+  </div>
+  </>
+);
+
+export default ConstructorPage;
