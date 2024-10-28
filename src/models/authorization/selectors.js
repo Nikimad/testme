ad5ef76d94f21886381aec799068a6dc019a1372
Nikimad <PybePoiD@gmail.com>
2024-10-28 12:47:54 +0300

    Change src/models/authorization/selectors.js

--- src/models/authorization/selectors.js
+++ src/models/authorization/selectors.js
@@ -9,5 +9,5 @@ export const authorizationSelectors = {
   selectIsLoading: createSelector(rootSelector, ({ isLoading }) => isLoading),
   selectError: createSelector(rootSelector, ({ error }) => error),
   selectIsUserAuthorized: createSelector(rootSelector, ({ user }) => !!user),
-  selecIsUserAdmin: createSelector(rootSelector, ({ user }) => !!user.is_admin),
+  selecIsUserAdmin: createSelector(rootSelector, ({ user }) => !!user?.is_admin),
 };
