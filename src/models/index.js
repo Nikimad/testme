e814a712fdca2cc478d372d75e0232992bd06976
Nikimad <PybePoiD@gmail.com>
2024-10-28 14:21:39 +0300

    Change src/models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import constructor from "./constructor";
 import authorization from "./authorization";
 import tests from "./tests";
 import rootSaga from "./sagas";
@@ -9,6 +10,7 @@ export const makeStore = (preloadedState) => {
   const store = configureStore({
     preloadedState,
     reducer: {
+      constructor,
       authorization,
       tests,
     },
