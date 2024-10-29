2a7c813abb6aee12336a490e7e9a4a3a88f9c219
Nikimad <PybePoiD@gmail.com>
2024-10-29 16:11:01 +0300

    Change src/app/(home)/@user/test/(constructor)/[testId]/constructor/_components/Question

--- src/app/(home)/@user/test/(constructor)/[testId]/constructor/_components/Question/Question.jsx
+++ src/app/(home)/@user/test/(constructor)/[testId]/constructor/_components/Question/Question.jsx
@@ -4,22 +4,12 @@ import s from "./Question.module.scss";
 const Question = ({ question, isEdit, name, index, onEdit, onFinish }) => (
   <li className={s.question}>
     {!isEdit ? (
-      <>
-        <div className={s.question__controls}>
-          <h4>{question.title}</h4>
-          <button type="button" className="pill" onClick={onEdit}>
-            Edit
-          </button>
-        </div>
-        <div className={s.question__controls}>
-          <button type="button" className="pill">
-            Add answer
-          </button>
-          <button type="button" className="pill">
-            Delete
-          </button>
-        </div>
-      </>
+      <div className={s.question__controls}>
+        <h4>{question.title}</h4>
+        <button type="button" className="pill" onClick={onEdit}>
+          Edit
+        </button>
+      </div>
     ) : (
       <QuestionField name={name} i={index} onFinish={onFinish} />
     )}
