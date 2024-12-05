5a7e98937221702c086d6150d7b1b1051ae38a34
Nikimad <PybePoiD@gmail.com>
2024-12-05 23:13:22 +0300

    Add src/lib/getTests

---
+++ src/lib/getTests.js
@@ -0,0 +1,54 @@
+import { cache } from "react";
+import { headers } from "next/headers";
+import { api } from "./api";
+import normolizeData from "./normolizeData";
+import { notFound } from "next/navigation";
+
+const getTests = cache(async (cookiesString) => {
+  const url = new URL(headers().get("x-url"));
+  const query = url.searchParams.toString();
+
+  let testsStorage = [];
+  let error = null;
+
+  const [testsError, data] = await api.getTests({
+    query,
+    cookies: cookiesString,
+  });
+
+  error = testsError;
+
+  const pathnameArr = url.pathname.split("/").filter(Boolean);
+  const isTestPathname = pathnameArr.indexOf("test") === 0;
+  const testId = isTestPathname
+    ? Number(
+        pathnameArr[pathnameArr.indexOf("test") + 1] === "edit"
+          ? pathnameArr[pathnameArr.indexOf("test") + 2]
+          : pathnameArr[pathnameArr.indexOf("test") + 1]
+      )
+    : null;
+  const isTestFetched = testId && data?.tests.some(({ id }) => id == testId);
+
+  testsStorage =
+    !error && (isTestFetched || !testId) ? data.tests : testsStorage;
+
+  if (!isTestFetched && testId) {
+    const [testError, test] = await api.getTest({
+      id: testId,
+      cookies: cookiesString,
+    });
+
+    error = testError;
+
+    testsStorage = !error ? [test] : testsStorage;
+  }
+
+  return {
+    error,
+    query: isTestFetched || !testId ? query : null,
+    meta: data?.meta,
+    data: normolizeData(testsStorage),
+  };
+});
+
+export default getTests;
