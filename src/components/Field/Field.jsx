c2c1239d6d7279c6aa00784c2a70663b2cbe01fb
Nikimad <PybePoiD@gmail.com>
2024-12-05 22:50:52 +0300

    Change src/components/Field

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,24 +1,22 @@
-import cn from "classnames";
 import Input from "../Input";
-import Error from "../Error";
+import ErrorMessage from "../ErrorMessage";
 import s from "./Field.module.scss";
 
-const Field = ({ isLabelHidden, label, name, onRender, ...props }) => (
+const Field = ({ label, id, name, onRender, children, ...props }) => (
   <div className={s.field}>
-    <label
-      htmlFor={`${name}_field`}
-      className={cn(s.field__label, { ["visually-hidden"]: isLabelHidden })}
-    >
+    <label htmlFor={`${id}_field`} className={s.field__label}>
       {label}
     </label>
-    <Input
-      key={name}
-      id={`${name}_field`}
-      name={name}
-      aria-describedby={`${name}_field-error`}
-      {...props}
-    />
-    <Error name={name} id={`${name}_field-error`} />
+    <div className={s.field__inputcontainer}>
+      <Input
+        id={`${id}_field`}
+        name={name}
+        aria-describedby={`${id}_field-error`}
+        {...props}
+      />
+      {children}
+    </div>
+    <ErrorMessage name={name} id={`${id}_field-error`} className={s.field__error} />
   </div>
 );
 
