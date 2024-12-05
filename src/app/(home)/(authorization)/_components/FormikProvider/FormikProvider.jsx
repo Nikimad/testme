ad7cf249f7925003a49f90861a0d052189b2ee75
Nikimad <PybePoiD@gmail.com>
2024-12-05 22:40:19 +0300

    Change src/app/(home)/(authorization)/_components/FormikProvider

--- src/app/(home)/(authorization)/_components/FormikProvider/FormikProvider.jsx
+++ src/app/(home)/(authorization)/_components/FormikProvider/FormikProvider.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import * as Yup from "yup";
 import { usePathname } from "next/navigation";
 import { useAction } from "@/models/hooks";
 import { authorizationActions } from "@/models/authorization";
+import { getAuthorizationValidationSchema } from "@/lib/validation";
 import { Formik, Form } from "formik";
 
 const FormikProvider = ({ children }) => {
@@ -14,10 +14,6 @@ const FormikProvider = ({ children }) => {
   );
   const handleSubmit = (values) => submitAction(values);
 
-  const validateStringRequired = Yup.string().required(
-    "You can't leave this field blank"
-  );
-
   return (
     <Formik
       initialValues={{
@@ -27,27 +23,12 @@ const FormikProvider = ({ children }) => {
         password_confirmation: "",
       }}
       initialTouched={{ isSignup: true }}
-      validationSchema={Yup.object({
-        username: validateStringRequired,
-        password: validateStringRequired.when("isSignup", {
-          is: true,
-          then: (schema) =>
-            schema.min(6, "Password must be at least 6 characters"),
-        }),
-        password_confirmation: Yup.mixed().when("isSignup", {
-          is: true,
-          then: () =>
-            validateStringRequired.oneOf(
-              [Yup.ref("password")],
-              "Password confirmation must match password"
-            ),
-        }),
-      })}
+      validationSchema={getAuthorizationValidationSchema()}
       validateOnBlur={false}
       validateOnChange={false}
       onSubmit={handleSubmit}
     >
-      <Form>{children}</Form>
+      <Form className="form-grid">{children}</Form>
     </Formik>
   );
 };
