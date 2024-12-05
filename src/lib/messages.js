63445d73fafe46ec03f0c18ff146b191c741b9ee
Nikimad <PybePoiD@gmail.com>
2024-12-05 23:00:40 +0300

    Change src/lib/messages.js

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -1,1 +1,6 @@
-export const REQUIERD_STRING = "Can't be blank";
+export const REQUIERD = "Can't be blank";
+export const NUMBER = "Must be a number";
+export const MIN_PASSWORD = (n) => `Password must be atleast ${n} characters`;
+export const PASSWORD_MATCH = "Password confirmation must match password";
+export const RIGHTANSWER_REQUIRED = "Question must have atleast one right answer";
+export const UNIQUE_ANSWER = "Answer must be unique";
