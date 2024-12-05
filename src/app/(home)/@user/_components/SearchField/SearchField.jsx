dd09d04193d16a98bb1c394416b86d5318ab6544
Nikimad <PybePoiD@gmail.com>
2024-12-05 22:42:21 +0300

    Change src/app/(home)/@user/_components/SearchField

--- src/app/(home)/@user/_components/SearchField/SearchField.jsx
+++ src/app/(home)/@user/_components/SearchField/SearchField.jsx
@@ -1,38 +1,17 @@
-"use client";
+import SearchbarField from "../SearchbarField";
+import SearchbarLabel from "../SearchbarLabel";
+import SearchbarInput from "../SearchbarInput";
 
-import useSearchState from "@/hooks/useSearchState";
-import cn from "classnames";
-
-const SearchField = ({
-  name,
-  defaultValue,
-  className,
-  enableEmpty,
-  ...props
-}) => {
-  const { value, valueComparer, searchParam, onChange, onBlur } = useSearchState(
-    name,
-    defaultValue || props.value,
-    enableEmpty,
-  );
-
-  const handleChange = ({ target: { value } }) => onChange(value, "replace");
-  const handleBlur = ({ target: { value } }) => onBlur(value, "replace");
-
-  return (
-    <input
-      {...props}
-      ref={valueComparer}
-      name={name}
-      className={cn("input", className ?? "")}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      value={value}
-      {...(props.type === "radio" && {
-        checked: value === (searchParam || defaultValue),
-      })}
+const SearchField = () => (
+  <SearchbarField>
+    <SearchbarLabel htmlFor="search">Search</SearchbarLabel>
+    <SearchbarInput
+      id="search"
+      name="search"
+      type="search"
+      enableEmpty={true}
     />
-  );
-};
+  </SearchbarField>
+);
 
 export default SearchField;
