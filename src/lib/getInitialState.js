6bf513b27bc57c7a40688eb628d468a906404565
Nikimad <PybePoiD@gmail.com>
2024-12-05 22:57:44 +0300

    Change src/lib/getInitialState.js

--- src/lib/getInitialState.js
+++ src/lib/getInitialState.js
@@ -1,43 +1,36 @@
-import { cookies, headers } from "next/headers";
-import { authorization, tests } from "@/lib/api";
-import { initialState as defaultState } from "./initialState";
+"use server";
+
+import { cookies } from "next/headers";
+import { defaultState } from "./defaultState";
+import getUser from "./getUser";
+import getTests from "./getTests";
 
 const getInitialState = async () => {
-  const url = new URL(headers().get("x-url"));
-  const pathnameArr = url.pathname.split("/").filter(Boolean);
-
-  const testId = Number(pathnameArr[pathnameArr.indexOf("test") + 1]);
-  const query = url.searchParams.toString();
   const cookiesString = cookies().toString();
 
-  const initialState = { ...defaultState };
+  const initialState = {
+    authorization: { ...defaultState.authorization },
+    tests: { ...defaultState.tests },
+    questions: { ...defaultState.questions },
+    answers: { ...defaultState.answers },
+  };
 
-  try {
-    const user = await authorization.getUser(cookiesString);
+  const user = await getUser(cookiesString);
+
+  if (user) {
     initialState.authorization.user = user;
-  } catch {}
 
-  if (initialState.authorization.user) {
-    try {
-      if (testId) {
-        const test = await tests.getTest({ id: testId, cookies: cookiesString });
-        initialState.tests.tests = [test];
-        if (initialState.authorization.user.is_admin && url.pathname.endsWith("/constructor")) {
-          initialState.constructor.id = test.id;
-          initialState.constructor.title = test.title;
-          initialState.constructor.questions = test.questions;
-        }
-      }
-      if (!testId) {
-        initialState.tests.query = query;
-        const data = await tests.getTests({ query, cookies: cookiesString });
-        console.log(data);
-        initialState.tests.meta = data.meta;
-        initialState.tests.tests = data.tests;
-      }
-    } catch ({ error }) {
-      initialState.tests.error = error;
-    }
+    const { error, query, meta, data } = await getTests(cookiesString);
+
+    initialState.tests.error = error
+    initialState.tests.query = query;
+    initialState.tests.meta = meta || initialState.tests.meta;
+    initialState.tests.entities = data.tests.entities;
+    initialState.tests.ids = data.tests.ids;
+    initialState.questions.entities = data.questions.entities;
+    initialState.questions.ids = data.questions.ids;
+    initialState.answers.entities = data.answers.entities;
+    initialState.answers.ids = data.answers.ids;
   }
 
   return initialState;
