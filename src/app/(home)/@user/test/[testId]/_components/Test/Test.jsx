2805797fd0c197c5930bcd27138ef0f7e916de1f
Nikimad <PybePoiD@gmail.com>
2024-12-05 22:46:42 +0300

    Change src/app/(home)/@user/test/[testId]/_components/Test

--- src/app/(home)/@user/test/[testId]/_components/Test/Test.jsx
+++ src/app/(home)/@user/test/[testId]/_components/Test/Test.jsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { useParams } from "next/navigation";
 import { useAppSelector } from "@/models/hooks";
 import { testsSelectors } from "@/models/tests/selectors";
+import { questionsSelectors } from "@/models/questions/selectors";
 import { Formik, Form } from "formik";
 import Question from "../Question";
 import Controls from "../Controls";
 import s from "./Test.module.scss";
+import ErrorPage from "@/components/ErrorPage";
 
-const Test = () => {
-  const { testId } = useParams();
+const Test = ({ params: { testId } }) => {
+  const test = useAppSelector((state) =>
+    testsSelectors.selectById(state, testId)
+  );
+  const questions = useAppSelector(
+    questionsSelectors.selectAllByTestId(test?.id)
+  );
 
-  const test = useAppSelector(testsSelectors.selectById(testId));
-
-  const defaultState = test.questions.reduce(
+  const defaultState = questions.reduce(
     (state, { id }) => {
       state.initialValues[id] = "";
       state.initialTouched[id] = true;
@@ -25,17 +29,19 @@ const Test = () => {
     }
   );
 
-
-  return (
+  return testId != test?.id ? (
+    <ErrorPage text="Test not found" />
+  ) : (
     <Formik
       initialTouched={defaultState.initialTouched}
       initialValues={defaultState.initialValues}
       validateOnChange={false}
       validateOnBlur={false}
+      onSubmit={() => {}}
     >
       <Form className={s.test}>
         <h2 className={s.test__title}>{test.title}</h2>
-        {test.questions.map((question) => (
+        {questions.map((question) => (
           <Question key={question.id} question={question} />
         ))}
         <Controls />
