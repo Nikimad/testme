e42010f53d45895191949842b6806477c20ecee4
Nikimad <PybePoiD@gmail.com>
2024-12-06 15:28:24 +0300

    Change src/middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,8 @@
 import { NextResponse } from "next/server";
-import { api } from "./lib/api";
-import { cookies } from "next/headers";
 
 export async function middleware(request) {
   const url = new URL(request.url);
-  
-  if (url.pathname === "/signin" || url.pathname === "/signup") {
-    const [_, user] = await api.getUser(cookies().toString());
-    if (user) return NextResponse.redirect(new URL("/", request.url));
-  }
-  
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-url", url);
 
