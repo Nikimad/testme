89608ac09b8fa68dfd5eb938075cb5698a8abcff
Nikimad <PybePoiD@gmail.com>
2024-12-09 17:58:32 +0300

    Change src/app/(home)/@user/test/[testId]/_components/Result

--- src/app/(home)/@user/test/[testId]/_components/Result/Result.jsx
+++ src/app/(home)/@user/test/[testId]/_components/Result/Result.jsx
@@ -4,9 +4,9 @@ import { useFormikContext } from "formik";
 
 const Result = () => {
   const { values, errors } = useFormikContext();
-  const questionsCount = Object.values(values).length;
+  const questionsCount = Object.values(values).filter((val) => val !== null).length;
   const resultCount = questionsCount - Object.values(errors).length;
-  return <>{(resultCount / questionsCount) * 100}%</>;
+  return <>{((resultCount / questionsCount) || 0) * 100}%</>;
 };
 
 export default Result;
