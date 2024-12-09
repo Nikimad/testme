a7c644e904a5ab619a821197aa239678ea76b9b3
Nikimad <PybePoiD@gmail.com>
2024-12-09 17:57:26 +0300

    Change src/app/(home)/@user/test/[testId]/_components/Answers

--- src/app/(home)/@user/test/[testId]/_components/Answers/Answers.jsx
+++ src/app/(home)/@user/test/[testId]/_components/Answers/Answers.jsx
@@ -1,20 +1,29 @@
-import BoxField from "@/components/BoxField";
+"use client";
+
+import cn from "classnames";
+import Choice from "@/components/Choice";
+import { Field } from "formik";
 import s from "./Answers.module.scss";
 
 const Answers = ({ name, type, answers, validate }) => (
   <fieldset className={s.answers} aria-describedby={`${name}-error`}>
     <legend className="visually-hidden">Answers</legend>
     {answers.map((answer) => (
-      <label key={answer.id} className={s.answers__answer}>
-        <BoxField
+      <Choice
+        key={answer.id}
+        label={answer.text}
+        wrapperClassName={s.answers__answer}
+        className={s.answers__answer__choice}
+      >
+        <Field
           name={name}
           id={`answer_${answer.id}`}
           type={type}
           value={answer.text}
           validate={validate}
+          className={cn("visually-hidden", s.answers__answer__input)}
         />
-        <span>{answer.text}</span>
-      </label>
+      </Choice>
     ))}
   </fieldset>
 );
